81b878f9fe88315a095e95fbc4a9c23455684fc1
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-11 22:07:02 +0500

    Added Token.cs

---
+++ SharpCalc/Token.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpCalc;
+
+public enum TokenType
+{
+    PLUS, 
+    MINUS,
+    DIVIDE, 
+    MULTIPLY
+}
+
+public class Token
+{
+    public Token(TokenType type, string value)
+    {
+        Type = type;
+        Value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public TokenType Type { get; private set; }
+    public string Value { get; private set; }  
+}
