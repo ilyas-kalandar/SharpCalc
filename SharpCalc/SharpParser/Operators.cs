7c30289c979f44ca3f2fc13da07d5389b9df43bb
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-13 22:36:12 +0500

    Added enum with operators

---
+++ SharpCalc/SharpParser/Operators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCalc.SharpParser
+{
+    /// <summary>
+    /// An Operators enum
+    /// </summary>
+    public enum Operator
+    {
+        PLUS,
+        MINUS,
+        DIVIDE,
+        MULTIPLY
+    }
+}
