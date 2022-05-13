e8a941c148f6b9c2648e23d6aa85acbac9db7fed
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-13 21:05:07 +0500

    Added documentation comments to Token.cs

---
+++ SharpCalc/SharpLexer/Token.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpCalc.SharLexer
+{
+    /// <summary>
+    /// A class for representing Token
+    /// </summary>
+    public class Token
+    {
+        /// <summary>
+        /// A default constructor
+        /// </summary>
+        /// <param name="type">Type of token</param>
+        /// <param name="value">Value of token (may be null)</param>
+        public Token(TokenType type, string? value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        /// <summary>
+        /// Get Type of token
+        /// </summary>
+        public TokenType Type { get; private set; }
+        /// <summary>
+        /// Get value (may be null!)
+        /// </summary>
+        public string? Value { get; private set; }
+
+        /// <summary>
+        /// Default override of object.ToString()
+        /// </summary>
+        /// <returns>A string representation of token</returns>
+        public override string ToString()
+        {
+            return $"Token(Type={Type}, Value=({Value}))";
+        }
+    }
+}
