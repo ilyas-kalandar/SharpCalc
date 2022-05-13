40aef4ed3592fb3e77cf46e71b09e9796532876f
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-13 22:37:03 +0500

    Added TreeLeaf.cs

---
+++ SharpCalc/SharpParser/TreeLeaf.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace SharpCalc.SharpParser
+{
+    /// <summary>
+    /// Class for representing a leaf of AST
+    /// </summary>
+    public class TreeLeaf : IAbstractSyntaxTree
+    {
+        private BigInteger _value;
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="val">An integer represented as string</param>
+        public TreeLeaf(string? val)
+        { 
+            if (!BigInteger.TryParse(val, out _value))
+            {
+                throw new ArgumentException($"Invalid argument for BigInteger provided! {val}");
+            }
+        }
+        /// <summary>
+        /// Returns a value of Leaf
+        /// </summary>
+        /// <returns></returns>
+        public BigInteger Evalulate()
+        {
+            return _value;
+        }
+    }
+}
