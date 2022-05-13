1b8c711db38dfa7360aa28419e725a39ccf45bf8
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-13 21:05:27 +0500

    Added documentation comments to TokenType.cs

---
+++ SharpCalc/SharpLexer/TokenType.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpCalc.SharLexer
+{
+    /// <summary>
+    /// A token types
+    /// </summary>
+    public enum TokenType
+    {
+        PLUS,
+        MINUS,
+        DIVIDE,
+        MULTIPLY,
+        INT_VALUE,
+        RIGHT_PAREN,
+        LEFT_PAREN
+    }
+}
