d021e7d74e0523f2da4eb3920a3304ecf2cc1f8c
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-13 22:37:22 +0500

    Added calculation cycle

--- SharpCalc/Program.cs
+++ SharpCalc/Program.cs
@@ -1,11 +1,40 @@
-﻿using SharpCalc.Lexer;
+﻿using SharpCalc.SharLexer;
+using SharpCalc.SharpParser;
 
-namespace SharpCalc;
-
-public class Program
+namespace SharpCalc
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        
+        public static void Main(string[] args)
+        {
+            Lexer lex;
+            Parser parser;
+            while(true)
+            {
+                Console.Write("SharpCalc>> ");
+                string? line = Console.ReadLine();
+
+                if (line == null || line.Length == 0)
+                {
+                    Environment.Exit(0);
+                }
+
+                lex = new Lexer(line);
+                lex.GetTokens();
+                parser = new Parser(lex.Tokens);
+                IAbstractSyntaxTree ast;
+
+                try
+                {
+                    ast = parser.Parse();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception occured: ", e.Message);
+                    continue;
+                }
+                Console.WriteLine($"Ans: {ast.Evalulate()}");
+            }
+        }
     }
 }
